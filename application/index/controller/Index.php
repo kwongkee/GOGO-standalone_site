790764e9a231b883442d521c6b35dbcb35306cec
Gogo198 <198@gogo198.net>
2025-11-07 09:58:04 +0800

    修改代码

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -2384,7 +2384,7 @@ class Index
             if ($page != 0) {
                 $page = $limit * $page;
             }
-            $where =['company_id'=>$company_id,'top_id'=>$top_id,'pid'=>$pid];
+            $where =['company_id'=>$company_id,'top_id'=>$top_id];
             $keyword = isset($dat['keywords']) ? trim($dat['keywords']) : '';
 
             $count = Db::connect($this->config)->name('guide_content')->where($where)->where('name', 'like', '%'.$keyword.'%')->count();
