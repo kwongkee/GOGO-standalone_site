9cea22f00b879215ae8898ad322e15584607d5b5
Gogo198 <198@gogo198.net>
2025-11-13 17:40:57 +0800

    更新代码

--- application/route.php
+++ application/route.php
@@ -14,7 +14,7 @@ use think\Route;
 Route::get('/', 'index/index/index'); // 定义GET请求路由规则
 //独立网站界面===========start
 //Route::get('index/website_manage', 'index/Index/website_manage');#企业管理
-Route::rule(':company_id/admin/?', 'index/index/website_manage');
+Route::rule(':company_id/admin/', 'index/index/website_manage');
 //    ->pattern(['company_id' => '\d+']);
 Route::any('index/get_enterprise_info', 'index/Index/get_enterprise_info');#获取企业信息
 Route::any('index/save_domainname', 'index/Index/save_domainname');#保存企业二级域名
@@ -200,13 +200,13 @@ Route::any('index/chat_association_info', 'index/Index/chat_association_info');#
 
 //**商家官网界面**====START
 //Route::get('index/merch_website_index', 'index/index/merch_website_index'); // 定义GET请求路由规则
-Route::rule(':company_id/web/?', 'index/index/merch_website_index');
+Route::rule(':company_id/web/', 'index/index/merch_website_index');
 //    ->pattern(['company_id' => '\d+']);
 //**商家官网界面**====END
 
 //**商家商城界面**====START
 //Route::get('merch/merch_shop_index', 'index/Merch/merch_shop_index'); // 定义GET请求路由规则
-Route::rule(':company_id/shop/?', 'index/merch/merch_shop_index');
+Route::rule(':company_id/shop/', 'index/merch/merch_shop_index');
 //    ->pattern(['company_id' => '\d+']);
 Route::any('merch/rate_detail', 'index/Merch/rate_detail'); // 定义GET请求路由规则
 Route::any('merch/detail', 'index/Merch/detail'); // 菜单详情
